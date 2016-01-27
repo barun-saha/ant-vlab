a7a8474d8d56cef8f13d411cd0affcd5c6384b8d
Soumenvls <mouliksoumen@gmail.com>
2016-01-27 13:45:50 +0530

    Upgraded pip; upgraded django version; removed needless packages

--- scripts/install_python_packages.sh
+++ scripts/install_python_packages.sh
@@ -25,25 +25,15 @@
 
 log 'Installing necessary Python packages'
 
-# Django version problem
-sudo -E pip install Django==1.4.2
-#sudo -E pip install Django==1.8.4
+# Upgrade pip
+sudo -E pip install -U setuptools
+sudo -E pip install -U pip
 
-## Library loading issues with pygraphviz
-## https://github.com/pygraphviz/pygraphviz/issues/71
-#sudo -E pip install pygraphviz --install-option="--include-path=/usr/include/graphviz" --install-option="--library-path=/usr/lib/graphviz/"
-
-# However, the above solution did not work for us
-# Other solution
-# https://github.com/pygraphviz/pygraphviz/issues/54
-sudo -E apt-get -y install graphviz
-sudo -E apt-get -y install graphviz-devel
-sudo -E pip install pygraphviz
+sudo -E pip install Django==1.8.4
 
 # Environment error while installing 'MySQL-python' package
 # http://stackoverflow.com/questions/5178292/pip-install-mysql-python-fails-with-environmenterror-mysql-config-not-found
 sudo -E apt-get -y install libmysqlclient-dev
-
 sudo -E pip install MySQL-python
 
 # Commented because it autimatically updates the Django to the newest version
