3fe246ee99c48c0a7edad61886835c37b4d52f9b
Soumenvls <mouliksoumen@gmail.com>
2016-01-27 13:54:16 +0530

    Added apache2 to the package installation list

--- scripts/install_ubuntu_packages.sh
+++ scripts/install_ubuntu_packages.sh
@@ -14,7 +14,7 @@ source ../scripts/common.sh
 
 log 'Installing necessary Ubuntu packages'
 
-sudo -E apt-get -y install libapache2-mod-wsgi
+sudo -E apt-get -y install apache2 libapache2-mod-wsgi
 
 # Enable WSGIScriptAlias for Apache2
 sudo a2enmod wsgi
@@ -69,4 +69,3 @@ sudo -E apt-get -y install --fix-missing
 
 sudo -E apt-get -y install build-essential libncurses5-dev openssl libssl-dev fop xsltproc unixodbc-dev
 sudo -E apt-get -y install --fix-missing
-
